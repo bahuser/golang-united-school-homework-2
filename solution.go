ba41bab9715e41e210094a0afd9f42abbe990704
Sergei Bakhaev <sergei.bakhaev@outlook.com>
2022-02-22 17:44:16 +0300

    Solution

--- solution.go
+++ solution.go
@@ -7,6 +7,25 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+import "math"
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type sidesNum int
+
+const (
+	SidesSquare   sidesNum = 4
+	SidesTriangle sidesNum = 3
+	SidesCircle   sidesNum = 0
+)
+
+func CalcSquare(sideLen float64, sidesNum sidesNum) float64 {
+	switch sidesNum {
+	case SidesSquare:
+		return sideLen * sideLen
+	case SidesTriangle:
+		return (sideLen * sideLen * math.Sqrt(3)) / 2
+	case SidesCircle:
+		return math.Pi * (sideLen * sideLen)
+	default:
+		return 0
+	}
 }
